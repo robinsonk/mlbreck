ff74cbbc907b939e83a78de4f015db02b89e55f0
robinsonk <kileen.robinson@gmail.com>
2021-07-19 05:27:16 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+    $('.fadein-onload').each( function(i){
+        $(this).animate({'opacity':'1'},1000);
+    }); 
+
+    
+    $(window).scroll( function(){
+        $('.fadeinright').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top - 650 + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-right':'0px'},1600);
+            }
+            
+        }); 
+    });
+
+
+    $(window).scroll( function(){
+        $('.fadeinright-sooner').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top - 150 + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-right':'0px'},1600);
+            }
+            
+        }); 
+    });
+
+    $(window).scroll( function(){
+        $('.fadeinleft-sooner').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top - 650 + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1600);
+            }
+            
+        }); 
+    });
+
+
+    $(window).scroll( function(){
+        $('.fadein').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},1700);
+            }
+            
+        }); 
+
+        $('.fadein-quick').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top - 250 + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},1400);
+            }
+            
+        }); 
+    });
+});
+
