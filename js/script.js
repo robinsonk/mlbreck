1f95894ca82aba32a77c14ae0e11f2382cbb155d
robinsonk <kileen.robinson@gmail.com>
2021-07-24 06:57:35 -0600

    final site structure done

--- js/script.js
+++ js/script.js
@@ -1,73 +1,66 @@
-$(document).ready(function() {
+function fadingElements() {
 
-    $('.fadein-onload').each( function(i){
-        $(this).animate({'opacity':'1'},1000);
-    }); 
-
-    
-    $(window).scroll( function(){
-        $('.fadeinright').each( function(i){
-            
-            var bottom_of_element = $(this).offset().top - 650 + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1','margin-right':'0px'},1600);
-            }
-            
-        }); 
+    $('.fadein-onload').each(function (i) {
+        $(this).animate({ 'opacity': '1' }, 1000);
     });
 
-
     $(window).scroll( function(){
-        $('.fadeinright-sooner').each( function(i){
-            
+        $('.fadeinright').each(function (i) {
+            var bottom_of_element = $(this).offset().top - 650 + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1', 'margin-right': '0px' }, 1600);
+            }
+        });
+
+
+        $('.fadeinright-sooner').each(function (i) {
             var bottom_of_element = $(this).offset().top - 150 + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1','margin-right':'0px'},1600);
-            }
-            
-        }); 
-    });
 
-    $(window).scroll( function(){
-        $('.fadeinleft-sooner').each( function(i){
-            
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1', 'margin-right': '0px' }, 1600);
+            }
+        });
+
+
+        $('.fadeinleft-sooner').each(function (i) {
             var bottom_of_element = $(this).offset().top - 650 + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1','margin-left':'0px'},1600);
+
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1', 'margin-left': '0px' }, 1600);
             }
-            
-        }); 
-    });
+        });
 
 
-    $(window).scroll( function(){
-        $('.fadein').each( function(i){
-            
+        $('.fadein').each(function (i) {
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1'},1700);
-            }
-            
-        }); 
 
-        $('.fadein-quick').each( function(i){
-            
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 1700);
+            }
+        });
+
+
+        $('.fadein-quick').each(function (i) {
             var bottom_of_element = $(this).offset().top - 250 + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1'},1400);
-            }
-            
-        }); 
-    });
-});
 
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 1400);
+            }
+        });
+    });
+};
+
+
+
+
+
+
+
+
+$(fadingElements);
